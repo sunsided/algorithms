d5e04294d334d7a9b11a886a3035a064603ee9c6
Markus Mayer <markus.mayer@derixx.de>
2015-06-26 13:52:10 +0200

    added quickselect algorithm

---
+++ src/Quickselect/Quickselect.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace widemeadows.algorithms
+{
+    /// <summary>
+    /// Class Quickselect.
+    /// </summary>
+    public sealed class Quickselect<TElement>
+        where TElement : IComparable
+    {
+        /// <summary>
+        /// The comparer
+        /// </summary>
+        [NotNull]
+        private readonly IComparer<TElement> _comparer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quickselect{TElement}"/> class.
+        /// </summary>
+        /// <param name="comparer">The comparer.</param>
+        public Quickselect([CanBeNull] IComparer<TElement> comparer = null)
+        {
+            _comparer = comparer ?? Comparer<TElement>.Default;
+        }
+
+        /// <summary>
+        /// Swaps the <paramref name="list"/> element at the <paramref name="sourceIndex"/> with the element
+        /// at the <paramref name="targetIndex"/>.
+        /// </summary>
+        /// <typeparam name="TList">The type of the list.</typeparam>
+        /// <typeparam name="TElement">The type of the element.</typeparam>
+        /// <param name="list">The list.</param>
+        /// <param name="sourceIndex">Index of the source.</param>
+        /// <param name="targetIndex">Index of the target.</param>
+        private void SwapElementAt<TList>([NotNull] TList list, int sourceIndex, int targetIndex)
+            where TList : IList<TElement>
+        {
+            var temp = list[targetIndex];
+            list[targetIndex] = list[sourceIndex];
+            list[sourceIndex] = temp;
+        }
+
+        /// <summary>
+        /// Rearranges (i.e. groups) the <paramref name="list"/> into two parts, where the left part
+        /// contains all elements smaller than the element at the <paramref name="pivotIndex"/> and the
+        /// right part contains all elements greater than or equal to it.
+        /// </summary>
+        /// <typeparam name="TList">The type of the list.</typeparam>
+        /// <typeparam name="TElement">The type of the element.</typeparam>
+        /// <param name="list">The list.</param>
+        /// <param name="leftIndex">Start index in the list.</param>
+        /// <param name="rightIndex">End index in the list.</param>
+        /// <param name="pivotIndex">Index of the pivot element.</param>
+        private int Partition<TList>([NotNull] TList list, int leftIndex, int rightIndex, int pivotIndex)
+            where TList : IList<TElement>
+        {
+            Debug.Assert(pivotIndex >= 0 && pivotIndex < list.Count, "pivotIndex >= 0 && pivotIndex < list.Count");
+            Debug.Assert(leftIndex >= 0 && leftIndex < list.Count,   "leftIndex >= 0 && leftIndex < list.Count");
+            Debug.Assert(rightIndex >= 0 && rightIndex < list.Count, "rightIndex >= 0 && rightIndex < list.Count");
+
+            // prefetch instance variables
+            var comparer = _comparer;
+
+            // fetch the pivot element and move it to the end of the list
+            var pivotValue = list[pivotIndex];
+            SwapElementAt(list, pivotIndex, rightIndex);
+
+            // partially sort the list
+            var storeIndex = leftIndex;
+            for (int i = leftIndex; i < rightIndex; ++i)
+            {
+                var element = list[i];
+                var elementIsSmallerThanPivot = comparer.Compare(element, pivotValue) < 0;
+                if (!elementIsSmallerThanPivot) continue;
+
+                SwapElementAt(list, storeIndex, i);
+                ++storeIndex;
+            }
+
+            // move pivot to the final place and return the store index
+            SwapElementAt(list, rightIndex, storeIndex);
+            return storeIndex;
+        }
+
+        /// <summary>
+        /// Selects a (random) pivot element between the <paramref name="leftIndex" /> and the <paramref name="rightIndex" />.
+        /// </summary>
+        /// <param name="leftIndex">Start index in the list.</param>
+        /// <param name="rightIndex">End index in the list.</param>
+        /// <returns>The pivot index.</returns>
+        private int SelectPivotElement(int leftIndex, int rightIndex)
+        {
+            var random = new Random();
+            return random.Next(leftIndex, rightIndex + 1);
+        }
+
+        /// <summary>
+        /// Selects the (<paramref name="n" />+1)-th smallest element in the <paramref name="list" />.
+        /// <para>
+        /// As a side effect, returns a partially sorted list, where each element left of the element
+        /// is smaller and each element right of it is greater or equal.
+        /// </para>
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <param name="n">The order (e.g. smallest element (<c>n=0</c>), 2nd smallest element (<c>n=1</c>), 3rd smallest (<c>n=2</c>), ...).</param>
+        /// <returns>The index of the <c>(n+1)</c>-th smallest element.</returns>
+        public int SelectRecursive<TList>([NotNull] TList list, int n)
+            where TList : IList<TElement>
+        {
+            return SelectRecursive(list, 0, list.Count-1, n);
+        }
+
+        /// <summary>
+        /// Selects the (<paramref name="n" />+1)-th smallest element in the <paramref name="list"/>
+        /// between the <paramref name="leftIndex"/> and the <paramref name="rightIndex"/>.
+        /// <para>
+        /// As a side effect, returns a partially sorted list, where each element left of the element
+        /// is smaller and each element right of it is greater or equal.
+        /// </para>
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <param name="leftIndex">Start index in the list.</param>
+        /// <param name="rightIndex">End index in the list.</param>
+        /// <param name="n">The order (e.g. smallest element (<c>n=0</c>), 2nd smallest element (<c>n=1</c>), 3rd smallest (<c>n=2</c>), ...).</param>
+        /// <returns>The index of the <c>(n+1)</c>-th smallest element.</returns>
+        private int SelectRecursive<TList>([NotNull] TList list, int leftIndex, int rightIndex, int n)
+            where TList : IList<TElement>
+        {
+            Debug.Assert(n >= 0 && n < list.Count, "pivotIndex >= 0 && pivotIndex < list.Count");
+            Debug.Assert(leftIndex >= 0 && leftIndex < list.Count, "leftIndex >= 0 && leftIndex < list.Count");
+            Debug.Assert(rightIndex >= 0 && rightIndex < list.Count, "rightIndex >= 0 && rightIndex < list.Count");
+
+            // when there only is one element, return it.
+            if (leftIndex == rightIndex) return leftIndex;
+
+            // select a pivot index and partition the list
+            var pivotIndex = SelectPivotElement(leftIndex, rightIndex);
+            pivotIndex = Partition(list, leftIndex, rightIndex, pivotIndex);
+
+            // check if the pivot index is at the final position
+            if (pivotIndex == n)
+            {
+                return pivotIndex;
+            }
+
+            // check if the pivot index is greater than the final position and
+            // recurse into the left half, otherwise the pivot is smaller or equal
+            // so we recurse into the right half.
+            return n < pivotIndex
+                ? SelectRecursive(list, leftIndex, pivotIndex - 1, n)
+                : SelectRecursive(list, pivotIndex + 1, rightIndex, n);
+        }
+    }
+}
---
+++ test-src/UnitTests/QuickselectTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace widemeadows.algorithms.tests
+{
+    [TestFixture]
+    public class QuickselectTests
+    {
+        /// <summary>
+        /// The length of the list
+        /// </summary>
+        private const int Length = 100;
+
+        /// <summary>
+        /// The list to test against
+        /// </summary>
+        private List<double> _list;
+
+        /// <summary>
+        /// Performs the test setup
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            var random = new Random();
+            var list = new List<double>(Length);
+            for (var i = 0; i < Length; ++i)
+            {
+                list.Add(random.NextDouble());
+            }
+            _list = list;
+        }
+
+        /// <summary>
+        /// Tests that the quickselect algorithm indeed returns the smallest element
+        /// </summary>
+        [Test]
+        public void QuickSelectFindsSmallestElement()
+        {
+            var list = _list;
+            var qs = new Quickselect<double>();
+
+            var smallestIndex = qs.SelectRecursive(list, 0);
+            smallestIndex.Should().Be(0, "because quickselect returns a partially ordered list left of the returned index which is smaller - and that list must be empty for the smallest element");
+
+            var smallestElement = list[smallestIndex];
+            smallestElement.Should().Be(list.Min(), "because that is the smallest element");
+        }
+
+        /// <summary>
+        /// Tests that the quickselect algorithm indeed returns the second-smallest element
+        /// </summary>
+        [Test]
+        public void QuickSelectFindsSecondSmallestElement()
+        {
+            var list = _list;
+            var qs = new Quickselect<double>();
+
+            var secondSmallestIndex = qs.SelectRecursive(list, 1);
+            secondSmallestIndex.Should().Be(1, "because quickselect returns a partially ordered list left of the returned index which is smaller - and there must only be one element to the left");
+
+            var secondSmallestElement = list[secondSmallestIndex];
+            secondSmallestElement.Should().BeGreaterThan(list[0], "because the element left of the returned index should be smaller");
+            secondSmallestElement.Should().BeLessOrEqualTo(list[2], "because the elements right of the returned index should be greater or equal");
+            list.Skip(2).Should().OnlyContain(value => value >= secondSmallestElement, "because all elements right of the returned index should be greater or equal");
+        }
+
+        /// <summary>
+        /// Tests that the quickselect algorithm indeed returns the n-th smallest element
+        /// </summary>
+        [Test]
+        public void QuickSelectFindsNthSmallestElement()
+        {
+            var list = _list;
+            var qs = new Quickselect<double>();
+
+            var n = Length/2;
+            var index = qs.SelectRecursive(list, n);
+            index.Should().BeInRange(0, Length - 1, "because we expect the result to be a list index");
+
+            var element = list[index];
+            list.Take(n).Should().OnlyContain(value => value < element, "because all elements left of the returned index should be smaller");
+            list.Skip(n).Should().OnlyContain(value => value >= element, "because all elements right of the returned index should be greater or equal");
+        }
+
+        /// <summary>
+        /// Tests that the quickselect algorithm indeed returns the smallest element
+        /// </summary>
+        [Test]
+        public void QuickSelectFindsGreatestElement()
+        {
+            var list = _list;
+            var qs = new Quickselect<double>();
+
+            var smallestIndex = qs.SelectRecursive(list, Length-1);
+            smallestIndex.Should().Be(Length-1, "because quickselect returns a partially ordered list right of the returned index which is greater or equal - and that list must be empty for the greatest element");
+
+            var smallestElement = list[smallestIndex];
+            smallestElement.Should().Be(list.Max(), "because that is the greatest element");
+        }
+    }
+}
